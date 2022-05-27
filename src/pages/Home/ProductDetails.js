59761dfb570a89051d753298373d76d64966c306
AlAminKh03 <alaminkhan6203@gmail.com>
2022-05-27 22:16:07 +0600

    several thing added

--- src/pages/Home/ProductDetails.js
+++ src/pages/Home/ProductDetails.js
@@ -18,7 +18,23 @@ const ProductDetails = () => {
     }
 
     return (
-        <div>
+        <div className='grid grid-cols-1 lg:grid-cols-2 mb-10'>
+            <div className='items-center mt-16'>
+                <div class="card w-96 bg-base-100 shadow-xl">
+                    <figure><img className='w-56' src={product.img} alt="Shoes" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title">
+                            {product.name}
+
+                        </h2>
+                        <p>{product.description}</p>
+                        <div class="card-actions justify-end">
+                            <div class="badge badge-outline">Price:{product.price}</div>
+                            <div class="badge badge-outline">Available:{product.available}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <form>
                 <div className="form-control w-full max-w-xs">
                     <label className="label">
@@ -41,10 +57,47 @@ const ProductDetails = () => {
                     />
 
                 </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                        <span className="label-text">Product Quantity</span>
+
+                    </label>
+                    <input type="number"
+                        className="input input-bordered w-full max-w-xs"
+                        {...register("number",
+
+                            {
+                                required: {
+                                    value: true,
+                                    message: 'quantity is required'
+                                },
+                                minLength: {
+                                    value: 3,
+                                    message: 'minimum order quantity 100'
+                                }
+                            })} />
+
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                        <span className="label-text">Phone Number</span>
+                    </label>
+                    <input type='number' className="input input-bordered w-full max-w-xs" {...register("phone number", { required: true })} />
+                </div>
+
+
+                <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                        <span className="label-text">Location</span>
+
+                    </label>
+                    <input type='text' className="input input-bordered w-full max-w-xs" {...register("phone number", { required: true })} />
+                </div>
+
+                <div className='form-control w-full max-w-xs mt-5'> <input className='btn w-50 max-w-xs' type="submit"
+                    value='Purchase' /></div>
             </form>
-            <div>
-                {product.name}
-            </div>
+
 
         </div>
     );
